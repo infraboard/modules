aa2dbb3875884248734da7230312a0dc4f441643
yumaojun03 <719118794@qq.com>
2024-05-14 19:33:15 +0800

    修改异常

--- iam/apps/token/exception.go
+++ iam/apps/token/exception.go
@@ -3,13 +3,13 @@ package token
 import "github.com/infraboard/mcube/v2/exception"
 
 func NewAuthFailed(format string, a ...any) *exception.APIException {
-	return exception.NewAPIException(5000, "Auth Failed", format, a...)
+	return exception.NewAPIException(5000, "Auth Failed").WithMessagef(format, a...)
 }
 
 func NewPermissionDeny(format string, a ...any) *exception.APIException {
-	return exception.NewAPIException(5100, "Permission Deny", format, a...)
+	return exception.NewAPIException(5100, "Permission Deny").WithMessagef(format, a...)
 }
 
 func NewTokenExpired(format string, a ...any) *exception.APIException {
-	return exception.NewAPIException(5001, "Token Expired", format, a...)
+	return exception.NewAPIException(5001, "Token Expired").WithMessagef(format, a...)
 }
