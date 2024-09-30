06d8eae13bc40fb84ae7c82fceee94f4e6cac766
yumaojun03 <719118794@qq.com>
2024-09-30 08:07:12 +0800

    修复依赖

--- iam/apps/token/exception.go
+++ iam/apps/token/exception.go
@@ -2,14 +2,14 @@ package token
 
 import "github.com/infraboard/mcube/v2/exception"
 
-func NewAuthFailed(format string, a ...any) *exception.APIException {
-	return exception.NewAPIException(5000, "Auth Failed").WithMessagef(format, a...)
+func NewAuthFailed(format string, a ...any) *exception.ApiException {
+	return exception.NewApiException(5000, "Auth Failed").WithMessagef(format, a...)
 }
 
-func NewPermissionDeny(format string, a ...any) *exception.APIException {
-	return exception.NewAPIException(5100, "Permission Deny").WithMessagef(format, a...)
+func NewPermissionDeny(format string, a ...any) *exception.ApiException {
+	return exception.NewApiException(5100, "Permission Deny").WithMessagef(format, a...)
 }
 
-func NewTokenExpired(format string, a ...any) *exception.APIException {
-	return exception.NewAPIException(5001, "Token Expired").WithMessagef(format, a...)
+func NewTokenExpired(format string, a ...any) *exception.ApiException {
+	return exception.NewApiException(5001, "Token Expired").WithMessagef(format, a...)
 }
